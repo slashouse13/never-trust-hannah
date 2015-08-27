f99949cc8b4d809c2880a8456be2f1e0ac06d418
Gareth Hughes <g_github@flamescape.com>
2015-08-27 20:37:01 +0100

    j-bomb

---
+++ garrysmod/addons/for_allah/lua/autorun/jihad_resources.lua
@@ -0,0 +1,64 @@
+--AutoDL the jihad shtuff :3
+
+--snds
+resource.AddFile( "sound/siege/jihad.wav" );
+resource.AddFile( "sound/siege/big_explosion.wav" );
+
+--icon
+resource.AddFile( "materials/vgui/entities/weapon_jihadbomb.vtf" );
+resource.AddFile( "materials/vgui/entities/weapon_jihadbomb.vmt" );
+
+--model
+resource.AddFile( "models/weapons/v_jb.mdl" );
+resource.AddFile( "models/weapons/v_jb.dx80" );
+resource.AddFile( "models/weapons/v_jb.dx90" );
+resource.AddFile( "models/weapons/v_jb.sw.vtx" );
+resource.AddFile( "models/weapons/v_jb.vvd" );
+resource.AddFile( "models/weapons/w_jb.mdl" );
+resource.AddFile( "models/weapons/w_jb.dx80" );
+resource.AddFile( "models/weapons/w_jb.dx90" );
+resource.AddFile( "models/weapons/w_jb.sw.vtx" );
+resource.AddFile( "models/weapons/w_jb.vvd" );
+resource.AddFile( "models/weapons/w_jb.phy" );
+
+--materials
+
+--vmodel
+resource.AddFile( "materials/models/weapons/v_models/pr0d.c4/bomb1.vmt" );
+resource.AddFile( "materials/models/weapons/v_models/pr0d.c4/bomb1.vtf" );
+resource.AddFile( "materials/models/weapons/v_models/pr0d.c4/bomb1_planted.vtf" );
+resource.AddFile( "materials/models/weapons/v_models/pr0d.c4/bomb1_ref.vtf" );
+resource.AddFile( "materials/models/weapons/v_models/pr0d.c4/bomb2.vmt" );
+resource.AddFile( "materials/models/weapons/v_models/pr0d.c4/bomb2.vtf" );
+resource.AddFile( "materials/models/weapons/v_models/pr0d.c4/bomb3b.vmt" );
+resource.AddFile( "materials/models/weapons/v_models/pr0d.c4/bomb3b.vtf" );
+resource.AddFile( "materials/models/weapons/v_models/pr0d.c4/and.vmt" );
+resource.AddFile( "materials/models/weapons/v_models/pr0d.c4/hand.vtf" );
+resource.AddFile( "materials/models/weapons/v_models/pr0d.c4/screen_04.vmt" );
+resource.AddFile( "materials/models/weapons/v_models/pr0d.c4/screen_04.vtf" );
+resource.AddFile( "materials/models/weapons/v_models/pr0d.c4/screen_45.vmt" );
+resource.AddFile( "materials/models/weapons/v_models/pr0d.c4/screen_45.vtf" );
+resource.AddFile( "materials/models/weapons/v_models/pr0d.c4/screen_active.vmt" );
+resource.AddFile( "materials/models/weapons/v_models/pr0d.c4/screen_active.vtf" );
+resource.AddFile( "materials/models/weapons/v_models/pr0d.c4/screen_off.vmt" );
+resource.AddFile( "materials/models/weapons/v_models/pr0d.c4/screen_off.vtf" );
+resource.AddFile( "materials/models/weapons/v_models/pr0d.c4/screen_off_ref.vtf" );
+resource.AddFile( "materials/models/weapons/v_models/pr0d.c4/screen_ref.vtf" );
+resource.AddFile( "materials/models/weapons/v_models/pr0d.c4/screen_on.vmt" );
+resource.AddFile( "materials/models/weapons/v_models/pr0d.c4/screen_on.vtf" );
+resource.AddFile( "materials/models/weapons/v_models/pr0d.c4/screen_on_ref.vtf" );
+
+--wmodel
+
+resource.AddFile( "materials/models/weapons/w_models/pr0d.c4/bomb1.vmt" );
+resource.AddFile( "materials/models/weapons/v_models/pr0d.c4/bomb1_planted.vmt" );
+resource.AddFile( "materials/models/weapons/w_models/pr0d.c4/bomb2.vmt" );
+resource.AddFile( "materials/models/weapons/w_models/pr0d.c4/bomb3b.vmt" );
+resource.AddFile( "materials/models/weapons/w_models/pr0d.c4/hand.vmt" );
+resource.AddFile( "materials/models/weapons/w_models/pr0d.c4/screen_04.vmt" );
+resource.AddFile( "materials/models/weapons/w_models/pr0d.c4/screen_45.vmt" );
+resource.AddFile( "materials/models/weapons/w_models/pr0d.c4/screen_active.vmt" );
+resource.AddFile( "materials/models/weapons/w_models/pr0d.c4/screen_on.vmt" );
+resource.AddFile( "materials/models/weapons/w_models/pr0d.c4/screen_off.vmt" );
+
+--Das all.
---
+++ garrysmod/addons/for_allah/lua/weapons/weapon_jihadbomb/shared.lua
@@ -0,0 +1,136 @@
+if SERVER then
+   AddCSLuaFile( "shared.lua" );
+   resource.AddFile("sound/siege/jihad.wav");
+   resource.AddFile("sound/siege/big_explosion.wav");
+end
+
+if CLIENT then
+   SWEP.PrintName                       = "Jihad"
+   SWEP.Slot                            = 7
+
+   SWEP.EquipMenuData = {
+      type  = "item_weapon",
+      name  = "Jihad Bomb",
+      desc  = "Left click goes boom!"
+   };
+
+   resource.AddFile("VGUI/ttt/icon_c4")
+   SWEP.Icon = "VGUI/ttt/icon_c4"
+end
+
+SWEP.Base = "weapon_tttbase"
+//SWEP.HoldType = "slam"
+SWEP.Kind = WEAPON_EQUIP2
+SWEP.CanBuy = { ROLE_TRAITOR }
+
+SWEP.Author			= "Stingwraith"
+SWEP.Contact		= "stingwraith123@yahoo.com"
+SWEP.Purpose		= "Sacrifice yourself for Allah."
+SWEP.Instructions	= "Left Click to make yourself EXPLODE. Right click to taunt."
+SWEP.DrawCrosshair		= false
+
+SWEP.Spawnable			= false
+SWEP.AdminSpawnable		= true		// Spawnable in singleplayer or by server admins
+
+resource.AddFile("models/weapons/v_c4.mdl")
+resource.AddFile("models/weapons/w_c4.mdl")
+SWEP.ViewModel			= "models/weapons/v_jb.mdl"
+SWEP.WorldModel			= "models/weapons/w_jb.mdl"
+
+SWEP.ViewModelFlip      = false
+
+SWEP.Primary.ClipSize		= -1
+SWEP.Primary.DefaultClip	= -1
+SWEP.Primary.Automatic		= false
+SWEP.Primary.Ammo			= "none"
+SWEP.Primary.Delay			= 5
+
+SWEP.Secondary.ClipSize		= -1
+SWEP.Secondary.DefaultClip	= -1
+SWEP.Secondary.Automatic	= false
+SWEP.Secondary.Ammo			= "none"
+
+/*---------------------------------------------------------
+	Reload does nothing
+---------------------------------------------------------*/
+function SWEP:Reload()
+end   
+
+function SWEP:Initialize()
+    util.PrecacheSound("siege/big_explosion.wav")
+    util.PrecacheSound("siege/jihad.wav")
+end
+
+
+/*---------------------------------------------------------
+   Think does nothing
+---------------------------------------------------------*/
+function SWEP:Think()	
+end
+
+
+/*---------------------------------------------------------
+	PrimaryAttack
+---------------------------------------------------------*/
+function SWEP:PrimaryAttack()
+    self.Weapon:SetNextPrimaryFire(CurTime() + 3)
+	
+	local effectdata = EffectData()
+		effectdata:SetOrigin( self.Owner:GetPos() )
+		effectdata:SetNormal( self.Owner:GetPos() )
+		effectdata:SetMagnitude( 8 )
+		effectdata:SetScale( 1 )
+		effectdata:SetRadius( 16 )
+	util.Effect( "Sparks", effectdata )
+	
+	self.BaseClass.ShootEffects( self )
+	
+	
+	-- The rest is only done on the server
+	if (SERVER) then
+		timer.Simple(2, function()
+            if IsValid(self) then
+                self:Asplode()
+            end
+        end)
+		self.Owner:EmitSound( "siege/jihad.wav" )
+	end
+
+end
+
+-- The asplode function
+function SWEP:Asplode()
+    -- if nobody is holding the bomb, it won't blow up
+    if not self.Owner or not IsValid(self.Owner) then return end
+    -- Make an explosion at your position
+	local ent = ents.Create("env_explosion")
+    ent:SetPos(self.Owner:GetPos())
+    ent:SetOwner(self.Owner)
+    ent:Spawn()
+    ent:SetKeyValue("iMagnitude", "250")
+    ent:Fire("Explode", 0, 0)
+    ent:EmitSound("siege/big_explosion.wav", 500, 100)
+    self.Owner:Kill()
+    -- no more salvaging exploded jihads
+    self:Remove()
+end
+
+
+/*---------------------------------------------------------
+	SecondaryAttack
+---------------------------------------------------------*/
+function SWEP:SecondaryAttack()	
+	
+	self.Weapon:SetNextSecondaryFire( CurTime() + 1 )
+	
+	local TauntSound = Sound( "vo/npc/male01/overhere01.wav" )
+
+	self.Weapon:EmitSound( TauntSound )
+	
+	// The rest is only done on the server
+	if (!SERVER) then return end
+	
+	self.Weapon:EmitSound( TauntSound )
+
+
+end
