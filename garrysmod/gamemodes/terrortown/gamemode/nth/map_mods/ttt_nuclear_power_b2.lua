2e107ae9e6b679c39655ee1d3ee3cd13e1405697
Gareth Hughes <g_github@flamescape.com>
2016-01-23 13:12:45 +0000

    map textures and stuff

---
+++ garrysmod/gamemodes/terrortown/gamemode/nth/map_mods/ttt_nuclear_power_b2.lua
@@ -0,0 +1,7 @@
+
+if SERVER then
+    NTH.AddFiles({
+        "materials/realworldtextures2/metal/vent_02.vmt",
+        "materials/realworldtextures2/metal/vent_02.vtf"
+    })
+end
---
+++ garrysmod/gamemodes/terrortown/gamemode/nth/map_mods/ttt_taiga_forest.lua
@@ -0,0 +1,11 @@
+
+if SERVER then
+    NTH.AddFiles({
+        "materials/maps/ttt_taiga_forest.vmt",
+        "materials/maps/ttt_taiga_forest.vtf",
+        "sound/ttt_taiga_forest/dance of the sugarplum fairy04.wav",
+        "sound/ttt_taiga_forest/dominick the donkey.wav",
+        "sound/ttt_taiga_forest/nutcracker overture.wav",
+        "sound/ttt_taiga_forest/the grinch.wav"
+    })
+end
